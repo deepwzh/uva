f088e47bb63e950476ce8d741c56c238b896b7c4
wzh2012@outlook.com <wzh2012@outlook.com>
2017-01-30 10:54:41 +0800

    一些更改

--- showproblem.php
+++ showproblem.php
@@ -13,84 +13,7 @@
         <title></title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/uva.css" rel="stylesheet">
-        <script></script>
-        <script type="text/javascript">
-        var page = 1;
-        var field,value,data;
-
-        function sort_table(elem){
-           // alert(document.getElementById(id).getElementsByTagName('span')[0].innerHTML);
-            var flag=elem.getElementsByTagName('span')[0];
-            if(flag.innerHTML == "-"){                    
-                var t = document.getElementById("ths").getElementsByTagName('th');
-                for(var i=2;i<t.length;i++){
-                    t[i].getElementsByTagName('span')[0].innerHTML = "";
-                }
-                flag.innerHTML="↑";
-                    query(1,"field=",1 + elem.id,'&'+window.data);
-            }
-            else{
-                if(flag.innerHTML=="↑"){
-                    flag.innerHTML="↓";
-                    query(1,"field=",2 + elem.id,'&'+window.data);
-                }
-                else if(flag.innerHTML=='↓'){
-                    flag.innerHTML="";
-                    query(1,"field=",0 + elem.id,'&'+window.data);
-
-                }
-            }
-
-        }
-        function query(page,title,x,str){
-            window.page =page;
-            window.field=title;
-            window.value=x;
-            var xmlhttp;
-            if(window.XMLHttpRequest){
-                xmlhttp=new XMLHttpRequest();
-            }
-            else{
-                xmlhttp =new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            xmlhttp.onreadystatechange=function(){
-                if(xmlhttp.readyState==4 && xmlhttp.status == 200){
-                    document.getElementById("tablecontent").innerHTML= xmlhttp.responseText;
-                }
-            }
-            xmlhttp.open("GET","showtable1.php?page="+ page+"&"+title + x+str,true);
-            //alert("showtable1.php?page="+ page+"&"+title + x+str);
-            xmlhttp.send();
-        }
-        function onload(){
-            query(1,window.field,window.value,'&'+window.data);
-            var ul = document.getElementById("page");
-            var lis=ul.getElementsByTagName('li');
-
-            for(var i=0;i<lis.length;i++){
-                lis[i].className="";
-                lis[i].onclick = function(){
-                    var liss=document.getElementById("page").getElementsByTagName('li');
-                    for(var i=0;i<liss.length;i++){
-                        liss[i].className = "";
-                    }
-                    this.className ='active';
-                    query(this.getElementsByTagName("a")[0].innerHTML,window.field,window.value,'&'+window.data);                    ;
-                }
-            }
-            lis[1].className='active';
-        }
-        function mysubmit(){
-            var form = $("form");
-            var data = form.serialize();
-            //alert(data);
-            window.data=data;
-            query(1,"","",data);
-            onload();
-            return false;
-        }
-
-        </script>
+        <script src="js/uva.js"></script>
     </head>
     <body  onload="onload()">
    	<a href="intro.txt" target="_blank">系统说明</a>
@@ -119,7 +42,6 @@
                 <option>Exercise</option>
             </select>
         </div>
-
         <div class="form-group">
             <label for="exampleInputEmail2">UVA题号</label>
             <input type="text" class="form-control" id="exampleInputEmail2" name="fuva_id" >
