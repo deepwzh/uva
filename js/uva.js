9b2f001d6d138bed05bad2d067d16a04e3434e87
wzh2012@outlook.com <wzh2012@outlook.com>
2017-01-30 11:06:47 +0800

    修改编码为gbk

--- js/uva.js
+++ js/uva.js
@@ -1,74 +1,74 @@
-        var page = 1;
-        var field,value,data;
+var page = 1;
+var field,value,data;
 
-        function sort_table(elem){
-           // alert(document.getElementById(id).getElementsByTagName('span')[0].innerHTML);
-            var flag=elem.getElementsByTagName('span')[0];
-            if(flag.innerHTML == "-"){                    
-                var t = document.getElementById("ths").getElementsByTagName('th');
-                for(var i=2;i<t.length;i++){
-                    t[i].getElementsByTagName('span')[0].innerHTML = "";
-                }
-                flag.innerHTML="↑";
-                    query(1,"field=",1 + elem.id,'&'+window.data);
-            }
-            else{
-                if(flag.innerHTML=="↑"){
-                    flag.innerHTML="↓";
-                    query(1,"field=",2 + elem.id,'&'+window.data);
-                }
-                else if(flag.innerHTML=='↓'){
-                    flag.innerHTML="";
-                    query(1,"field=",0 + elem.id,'&'+window.data);
-
-                }
-            }
+function sort_table(elem){
+    // alert(document.getElementById(id).getElementsByTagName('span')[0].innerHTML);
+    var flag=elem.getElementsByTagName('span')[0];
+    if(flag.innerHTML == "-"){                    
+        var t = document.getElementById("ths").getElementsByTagName('th');
+        for(var i=2;i<t.length;i++){
+            t[i].getElementsByTagName('span')[0].innerHTML = "";
+        }
+        flag.innerHTML="↑";
+            query(1,"field=",1 + elem.id,'&'+window.data);
+    }
+    else{
+        if(flag.innerHTML=="↑"){
+            flag.innerHTML="↓";
+            query(1,"field=",2 + elem.id,'&'+window.data);
+        }
+        else if(flag.innerHTML=='↓'){
+            flag.innerHTML="";
+            query(1,"field=",0 + elem.id,'&'+window.data);
 
         }
-        function query(page,title,x,str){
-            window.page =page;
-            window.field=title;
-            window.value=x;
-            var xmlhttp;
-            if(window.XMLHttpRequest){
-                xmlhttp=new XMLHttpRequest();
-            }
-            else{
-                xmlhttp =new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            xmlhttp.onreadystatechange=function(){
-                if(xmlhttp.readyState==4 && xmlhttp.status == 200){
-                    document.getElementById("tablecontent").innerHTML= xmlhttp.responseText;
-                }
-            }
-            xmlhttp.open("GET","showtable1.php?page="+ page+"&"+title + x+str,true);
-            //alert("showtable1.php?page="+ page+"&"+title + x+str);
-            xmlhttp.send();
-        }
-        function onload(){
-            query(1,window.field,window.value,'&'+window.data);
-            var ul = document.getElementById("page");
-            var lis=ul.getElementsByTagName('li');
+    }
 
-            for(var i=0;i<lis.length;i++){
-                lis[i].className="";
-                lis[i].onclick = function(){
-                    var liss=document.getElementById("page").getElementsByTagName('li');
-                    for(var i=0;i<liss.length;i++){
-                        liss[i].className = "";
-                    }
-                    this.className ='active';
-                    query(this.getElementsByTagName("a")[0].innerHTML,window.field,window.value,'&'+window.data);                    ;
-                }
+}
+function query(page,title,x,str){
+    window.page =page;
+    window.field=title;
+    window.value=x;
+    var xmlhttp;
+    if(window.XMLHttpRequest){
+        xmlhttp=new XMLHttpRequest();
+    }
+    else{
+        xmlhttp =new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function(){
+        if(xmlhttp.readyState==4 && xmlhttp.status == 200){
+            document.getElementById("tablecontent").innerHTML= xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET","showtable1.php?page="+ page+"&"+title + x+str,true);
+    //alert("showtable1.php?page="+ page+"&"+title + x+str);
+    xmlhttp.send();
+}
+function onload(){
+    query(1,window.field,window.value,'&'+window.data);
+    var ul = document.getElementById("page");
+    var lis=ul.getElementsByTagName('li');
+
+    for(var i=0;i<lis.length;i++){
+        lis[i].className="";
+        lis[i].onclick = function(){
+            var liss=document.getElementById("page").getElementsByTagName('li');
+            for(var i=0;i<liss.length;i++){
+                liss[i].className = "";
             }
-            lis[1].className='active';
+            this.className ='active';
+            query(this.getElementsByTagName("a")[0].innerHTML,window.field,window.value,'&'+window.data);                    ;
         }
-        function mysubmit(){
-            var form = $("form");
-            var data = form.serialize();
-            //alert(data);
-            window.data=data;
-            query(1,"","",data);
-            onload();
-            return false;
-        }
+    }
+    lis[1].className='active';
+}
+function mysubmit(){
+    var form = $("form");
+    var data = form.serialize();
+    //alert(data);
+    window.data=data;
+    query(1,"","",data);
+    onload();
+    return false;
+}
